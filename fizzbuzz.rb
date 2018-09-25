337d9cc5be467eafd6d1ba5c88c17b96c1901632
David Sim <wsim903@gmail.com>
2018-09-25 18:55:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(num)
+  out = ''
+  if fizzbuzz%3 == 0
+    out+='fizz'
+  elsif fizzbuzz%5 == 0
+    out+='buzz'
+  end
+  
+  if out == ''
+    out = nil
+  end
+  
+  return nil
+end
