1e7ee15297fe1f9b65b3b33344d5bf55593d8925
David Sim <wsim903@gmail.com>
2018-09-25 19:00:24 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num%3 == 0
+    out='Fizz'
+    if num%5 == 0
+      out+='Buzz'
+    end
+  elsif num%5 == 0
+    out='Buzz'
+  else
+    return nil
+  end
+  
+  return out
+end
